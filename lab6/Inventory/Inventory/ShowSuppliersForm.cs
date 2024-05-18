21f261b4fc8077148630b2d4aa1adef7ef4ed161
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-05-18 22:34:53 +0300

    added page for Postachalnik

---
+++ lab6/Inventory/Inventory/ShowSuppliersForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using ClassLibrary;
+
+namespace Inventory
+{
+    public partial class ShowSuppliersForm : Form
+    {
+        private MainController mainController;
+
+        public ShowSuppliersForm(MainController mainController)
+        {
+            InitializeComponent();
+            this.mainController = mainController;
+            LoadSuppliers();
+        }
+
+        private void ShowSuppliersForm_Load(object sender, EventArgs e)
+        {
+            LoadSuppliers();
+        }
+
+        private void LoadSuppliers()
+        {
+            var suppliers = mainController.GetAllSuppliers();
+            dgvSuppliers.DataSource = suppliers;
+        }
+
+        private void dgvSuppliers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            LoadSuppliers();
+        }
+    }
+}
