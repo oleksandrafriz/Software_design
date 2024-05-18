a833429a3d993ec7b1f3abbc9d55890bedd4d323
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-05-18 15:31:19 +0300

    added page 'ShowTovar'

---
+++ lab6/Inventory/Inventory/ShowTovar.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Inventory
+{
+    partial class ShowTovar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(237, 43);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(94, 29);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "Пошук";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(82, 391);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(94, 29);
+            this.deleteButton.TabIndex = 1;
+            this.deleteButton.Text = "Видалити";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(603, 391);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(94, 29);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "Оновити";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(67, 43);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(125, 27);
+            this.searchTextBox.TabIndex = 3;
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(67, 110);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowHeadersWidth = 51;
+            this.dgvProducts.RowTemplate.Height = 29;
+            this.dgvProducts.Size = new System.Drawing.Size(630, 232);
+            this.dgvProducts.TabIndex = 4;
+            // 
+            // ShowTovar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.searchButton);
+            this.Name = "ShowTovar";
+            this.Text = "ShowTovar";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.DataGridView dgvProducts;
+    }
+}
---
+++ lab6/Inventory/Inventory/ShowTovar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary;
+
+namespace Inventory
+{
+    public partial class ShowTovar : Form
+    {
+        private MainController mainController;
+        public ShowTovar(MainController mainController)
+        {
+            InitializeComponent();
+            this.mainController = mainController;
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            var products = mainController.GetAllProducts();
+            dgvProducts.DataSource = products;
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (dgvProducts.SelectedRows.Count > 0)
+            {
+                var productId = (int)dgvProducts.SelectedRows[0].Cells[0].Value;
+                mainController.DeleteProduct(productId);
+                LoadProducts();
+                MessageBox.Show("Product deleted successfully!");
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            var products = mainController.GetAllProducts()
+                                    .Where(p => p.Name.Contains(searchTextBox.Text))
+                                    .ToList();
+            dgvProducts.DataSource = products;
+        }
+    }
+}
