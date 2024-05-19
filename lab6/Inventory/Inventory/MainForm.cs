c5ac627c28c897aafc602e59cde42808c6a8a7f2
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-05-19 21:01:20 +0300

    checked for code smells and added changes in MainForm

--- lab6/Inventory/Inventory/MainForm.cs
+++ lab6/Inventory/Inventory/MainForm.cs
@@ -10,20 +10,30 @@ namespace Inventory
 {
     public partial class MainForm : Form
     {
-        private MainController mainController;
-        private int selectedProductId;
-
+        private readonly MainController _mainController;
+        private int _selectedProductId;
 
         public MainForm(MainController mainController)
         {
             InitializeComponent();
-            this.mainController = mainController;
+            _mainController = mainController ?? throw new ArgumentNullException(nameof(mainController));
             LoadProductIds();
+            InitializeSearchResultsGridView();
+        }
+
+        private void InitializeSearchResultsGridView()
+        {
+            searchResultsDataGridView.Columns.Clear();
+            searchResultsDataGridView.Columns.Add("Id", "ID");
+            searchResultsDataGridView.Columns.Add("Name", "Назва");
+            searchResultsDataGridView.Columns.Add("Quantity", "Кількість");
+            searchResultsDataGridView.Columns.Add("Price", "Ціна");
+            searchResultsDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
         }
 
         private void LoadProductIds()
         {
-            var products = mainController.GetAllProducts();
+            var products = _mainController.GetAllProducts();
             chooseTovar.DataSource = products;
             chooseTovar.DisplayMember = "Name";
             chooseTovar.ValueMember = "Id";
@@ -31,47 +41,35 @@ namespace Inventory
 
         private void addTovar_Click(object sender, EventArgs e)
         {
-            var addTovarPage = new AddTovarPage(mainController);
+            var addTovarPage = new AddTovarPage(_mainController);
             addTovarPage.Show();
         }
 
         private void editTovar_Click(object sender, EventArgs e)
         {
-            if (chooseTovar.SelectedItem != null)
+            if (chooseTovar.SelectedItem is Product selectedProduct)
             {
-                var selectedProduct = chooseTovar.SelectedItem as Product;
-                if (selectedProduct != null)
-                {
-                    selectedProductId = selectedProduct.Id;
-                    var editTovarPage = new EditTovarPage(mainController, selectedProductId);
-                    editTovarPage.Show();
-                }
-                else
-                {
-                    MessageBox.Show("Виберіть дійсний продукт.");
-                }
+                _selectedProductId = selectedProduct.Id;
+                var editTovarPage = new EditTovarPage(_mainController, _selectedProductId);
+                editTovarPage.Show();
             }
             else
             {
-                MessageBox.Show("Виберіть продукт для редагування.");
+                MessageBox.Show("Виберіть продукт для редагування.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void tovars_Click(object sender, EventArgs e)
         {
-            var showTovarPage = new ShowTovar(mainController);
+            var showTovarPage = new ShowTovar(_mainController);
             showTovarPage.Show();
         }
 
         private void chooseTovar_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (chooseTovar.SelectedItem != null)
+            if (chooseTovar.SelectedItem is Product selectedProduct)
             {
-                var selectedProduct = chooseTovar.SelectedItem as Product;
-                if (selectedProduct != null)
-                {
-                    selectedProductId = selectedProduct.Id;
-                }
+                _selectedProductId = selectedProduct.Id;
             }
         }
 
@@ -84,65 +82,58 @@ namespace Inventory
                 return;
             }
 
-            var products = mainController.SearchProductsByName(searchText);
+            var products = _mainController.SearchProductsByName(searchText);
+            DisplaySearchResults(products);
+        }
+
+        private void DisplaySearchResults(List<Product> products)
+        {
+            searchResultsDataGridView.Rows.Clear();
+
             if (products.Count == 0)
             {
                 MessageBox.Show("Не знайдено продуктів, що відповідають пошуковому терміну.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-
-            //chooseTovar.DataSource = products;
-            //chooseTovar.DisplayMember = "Name";
-            //chooseTovar.ValueMember = "Id";
-
-            searchResultsLabel.Text = "";
-
-            foreach (var product in products)
+            else
             {
-                searchResultsLabel.Text += $"ID: {product.Id},\n" +
-                                           $"Назва: {product.Name},\n" +
-                                           $"Кількість: {product.Quantity},\n" +
-                                           $"Ціна: {product.Price}{Environment.NewLine}";
+                foreach (var product in products)
+                {
+                    searchResultsDataGridView.Rows.Add(product.Id, product.Name, product.Quantity, product.Price);
+                }
             }
         }
 
         private void exportToCSVButton_Click(object sender, EventArgs e)
         {
-            var products = mainController.GetAllProducts();
+            var products = _mainController.GetAllProducts();
             using (var sfd = new SaveFileDialog())
             {
                 sfd.Filter = "CSV files (*.csv)|*.csv";
                 sfd.FileName = "Products.csv";
                 if (sfd.ShowDialog() == DialogResult.OK)
                 {
-                    using (var sw = new StreamWriter(sfd.FileName, false, new UTF8Encoding(true)))
-                    {
-                        sw.WriteLine("ID,Назва,Кількість,Ціна,Постачальник");
-                        foreach (var product in products)
-                        {
-                            sw.WriteLine($"{product.Id},{product.Name},{product.Quantity},{product.Price},{product.Postachalnik}");
-                        }
-                    }
-                    MessageBox.Show("Продукти успішно експортовані!");
+                    ExportProductsToCsv(sfd.FileName, products);
                 }
             }
         }
 
+        private void ExportProductsToCsv(string fileName, List<Product> products)
+        {
+            using (var sw = new StreamWriter(fileName, false, new UTF8Encoding(true)))
+            {
+                sw.WriteLine("ID,Назва,Кількість,Ціна,Постачальник");
+                foreach (var product in products)
+                {
+                    sw.WriteLine($"{product.Id},{product.Name},{product.Quantity},{product.Price},{product.Postachalnik}");
+                }
+            }
+            MessageBox.Show("Продукти успішно експортовані!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
 
         private void postachalniki_Click(object sender, EventArgs e)
         {
-            var showSuppliersForm = new ShowSuppliersForm(mainController);
+            var showSuppliersForm = new ShowSuppliersForm(_mainController);
             showSuppliersForm.Show();
         }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void searchResultsListBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
     }
 }
