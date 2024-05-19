8d1a58dcbba16cd08907358dc9edafc7bec3827f
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-05-19 18:26:17 +0300

    added changes for ShowSuppliersForm

--- lab6/Inventory/Inventory/ShowSuppliersForm.cs
+++ lab6/Inventory/Inventory/ShowSuppliersForm.cs
@@ -15,15 +15,15 @@ namespace Inventory
             LoadSuppliers();
         }
 
-        private void ShowSuppliersForm_Load(object sender, EventArgs e)
-        {
-            LoadSuppliers();
-        }
-
         private void LoadSuppliers()
         {
             var suppliers = mainController.GetAllSuppliers();
             dgvSuppliers.DataSource = suppliers;
+            dgvSuppliers.Columns["Id"].HeaderText = "ID Постачальника";
+            dgvSuppliers.Columns["Name"].HeaderText = "Ім'я Постачальника";
+            dgvSuppliers.Columns["Address"].HeaderText = "Адреса";
+            dgvSuppliers.Columns["PhoneNumber"].HeaderText = "Номер телефону";
+            dgvSuppliers.Columns["CompanyName"].HeaderText = "Назва компанії";
         }
 
         private void dgvSuppliers_CellContentClick(object sender, DataGridViewCellEventArgs e)
